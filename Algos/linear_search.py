bd8a17fb933fc868f62ce0c948eb872abfe2cdff
denmj <denismj87@gmail.com>
2020-05-18 14:32:36 -0500

    repository for Algos and problems

---
+++ Algos/linear_search.py
@@ -0,0 +1,17 @@
+# Linear saerch
+def search(arr, n , x):
+    for i in range(n):
+        if arr[i] == x:
+            return i
+    return -1
+
+
+a = [1, 10, 30, 15]
+x = 30
+n = len(a)
+
+# Worst Case Analysis  O(n)
+print(x, "is present at index",
+      search(a, n, x))
+
+
