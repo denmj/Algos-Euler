0bf06def617caaf3fee26cbc40ce13461816e37e
denmj <denismj87@gmail.com>
2020-05-20 10:24:54 -0500

    pr2

--- Algos/binary_search.py
+++ Algos/binary_search.py
@@ -7,7 +7,7 @@ def binary_search(arr, left, right, x):
         mid = left + (right - left) // 2
 
         if arr[mid] == x:
-            return mid
+            return mid, arr[mid]
         elif arr[mid] > x:
             return binary_search(arr, left, mid-1, x)
         else:
@@ -17,6 +17,7 @@ def binary_search(arr, left, right, x):
         return -1
 
 
-arr = [2,3,4,10,40,50]
+arr = [2, 3, 4, 10, 40, 50]
 
-binary_search(arr,0, len(arr)-1, 10)
+r, val = binary_search(arr,0, len(arr)-1, 10)
+print(r, val)
