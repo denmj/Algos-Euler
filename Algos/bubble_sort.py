75f8c10d62e0a90841d5abe33452b461ac416bfa
denmj <denismj87@gmail.com>
2020-09-07 03:51:09 +0000

    bubble

--- Algos/bubble_sort.py
+++ Algos/bubble_sort.py
@@ -1,17 +1,14 @@
-# Linear search
-def search(arr, n, x):
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+def bubbleSort(arr):
+    n = len(arr)
     for i in range(n):
-        if arr[i] == x:
-            return i
-    return -1
+        print("i", i)
+        for j in range(0, n - i - 1):
+            print("j", j)
+            pass
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[i]
 
 
-a = [1, 10, 30, 15]
-x = 30
-n = len(a)
-
-# Worst Case Analysis  O(n)
-print(x, "is present at index",
-      search(a, n, x))
-
-
+print(arr)
