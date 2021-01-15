2752d687446dd7aa47ca939639265db05463e386
denmj <denismj87@gmail.com>
2021-01-15 16:41:10 -0600

    cf_13

---
+++ Codefight/cf_13.py
@@ -0,0 +1,20 @@
+"""
+Write a function that reverses characters in (possibly nested) parentheses in the input string.
+
+Input strings will always be well-formed with matching ()s.
+
+For inputString = "(bar)", the output should be
+reverseInParentheses(inputString) = "rab";
+For inputString = "foo(bar)baz", the output should be
+reverseInParentheses(inputString) = "foorabbaz";
+"""
+
+
+def reverseInParentheses(s):
+    for i in range(len(s)):
+        if s[i] == "(":
+            start = i
+        if s[i] == ")":
+            end = i
+            return reverseInParentheses(s[:start] + s[start+1:end][::-1] + s[end+1:])
+    return s
