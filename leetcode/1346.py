253412b2f8ab386755c3eeff260aabc761978d8b
Denis Mamedov <denismj87@gmail.com>
2022-10-07 14:54:27 -0500

    Create 1346.py

---
+++ leetcode/1346.py
@@ -0,0 +1,9 @@
+from typing import Callable, List, Optional
+
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        for i in range(len(arr)):
+            for j in range(len(arr)):
+                if i != j and arr[i] == arr[j] * 2:
+                    return True
+        return False
