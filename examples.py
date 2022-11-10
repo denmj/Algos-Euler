7cd9a615336c292728b3632a12c2bca937440cda
Denis Mamedov <denismj87@gmail.com>
2022-11-10 10:56:00 -0600

    c

--- examples.py
+++ examples.py
@@ -18,10 +18,32 @@ A = np.array([[2104, 3, 400],
 X = A[:, 0:2]
 y = A[:, 2]
 
+# print("A shape of a X: ", A.shape)
+
+
+# norm of a vector
+v = np.linalg.norm(X)
+# print("magnitude of a matrix", v)
+
 # initialuze theta
 theta = np.zeros(2)
 
 # Normal equation
 theta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
 
-print(theta)
+# print("Coefficients theta0 and theta1 :", theta)
+
+
+# matrix with shape (3, 3)
+A = np.array([[1, 0, 0],
+            [-1, 1, 0],
+            [0, -1, 1]]) 
+
+x = np.array([1, 2, 3])
+
+b = A.dot(x)
+print(b)
+
+
+inv_A = np.linalg.inv(A)
+print(inv_A.dot(b))
