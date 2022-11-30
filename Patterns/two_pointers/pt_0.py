5ca86726e5c50fdc5cd7ff5d1a4ee82cb17e75a2
Denis Mamedov <denismj87@gmail.com>
2022-11-30 12:01:19 -0600

    tp_1

--- Patterns/two_pointers/pt_0.py
+++ Patterns/two_pointers/pt_0.py
@@ -1,8 +1,9 @@
-# Palindrome 
+from typing import List
+# Palindrome (E)
 
-s = "racecar"
-s2 = "race a car"
-s3 = 'A man, a plan, a canal: Panama'
+# s = "racecar"
+# s2 = "race a car"
+# s3 = 'A man, a plan, a canal: Panama'
 
 def is_palindrome(s: str) -> bool: 
     s = s.lower()
@@ -19,4 +20,38 @@ def is_palindrome(s: str) -> bool:
             right -= 1
     return True
 
+# (M)
 
+numbers = [11, 23, 2, 5, 7, 11, 15]
+target = 7
+
+def TwoSums(nums: List, target: int) -> List:
+
+
+    nums = sorted(nums)
+    left, right = 0, len(nums) - 1
+
+    while left < right:
+        sum = nums[left] + nums[right]
+        if sum == target:
+            return [left, right]
+        elif sum < target:
+            left += 1
+        else:
+            right -= 1
+    return []
+    
+
+
+
+
+
+
+p1, p2 = 0, len(numbers)-1
+
+while p1 < p2:
+    print("Index of : ", p1, "for: ", numbers[p1])
+    print("Index of : ", p2, "for: ", numbers[p2])
+    print("Sum of: ", numbers[p1], "and", numbers[p2], "is: ", numbers[p1] + numbers[p2])
+    p1 += 1 
+    p2 -= 1
