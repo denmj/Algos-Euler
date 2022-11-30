0c59b3a3fa3fae5187b66a8f0881f23fbef01141
denmj <denismj87@gmail.com>
2022-11-29 22:14:19 -0600

    lt-125

---
+++ leetcode/125.py
@@ -0,0 +1,16 @@
+#palindrome
+def is_palindrome(s: str) -> bool: 
+    s = s.lower()
+    left, right = 0, len(s) - 1
+    while left < right:
+        if not s[left].isalnum():
+            left += 1
+        elif not s[right].isalnum():
+            right -= 1
+        elif s[left] != s[right]:
+            return False
+        else:
+            left += 1
+            right -= 1
+    return True
+
