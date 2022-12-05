bc72c621f719c4d2be5a27392a1e9af80d906232
Denis Mamedov <denismj87@gmail.com>
2022-12-05 09:09:01 -0600

    1-167-lt

--- leetcode/1.py
+++ leetcode/1.py
@@ -20,15 +20,4 @@ class SolutionBrute:
                 if nums[i] + nums[j] == target:
                     return [i, j]
 
-class SolutionPT:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        left, right = 0, len(nums) - 1
-
-        while left < right:
-            s = nums[left] + nums[right]
-            if s == target:
-                return [left, right]
-            elif s < target:
-                left += 1
-            else:
-                right -= 1
+# 167 is the same as 1, but the array is sorted
