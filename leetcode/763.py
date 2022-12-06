de556b68ab7199b0ea878bb6e53660e5ae28f7f4
Denis Mamedov <denismj87@gmail.com>
2022-12-06 11:19:09 -0600

    lt-pt-42

--- leetcode/763.py
+++ leetcode/763.py
@@ -3,13 +3,14 @@ from typing import List
 class SolutionHashMap:
     def partitionLabels(self, S: str) -> List[int]:
         last = {c: i for i, c in enumerate(S)}
-        j = anchor = 0
+        j = size = 0
         ans = []
         for i, c in enumerate(S):
+            size += 1
             j = max(j, last[c])
             if i == j:
-                ans.append(i - anchor + 1)
-                anchor = i + 1
+                ans.append(size)
+                size = 0
         return ans
 
 # st = 'ababcbacadefegdehijhklij'
