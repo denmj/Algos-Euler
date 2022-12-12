92159821951135a81bfe9de8cb40f1b8f1927cdb
Denis Mamedov <denismj87@gmail.com>
2022-12-12 12:27:06 -0600

    lt-206-tp

--- leetcode/206.py
+++ leetcode/206.py
@@ -1,5 +1,6 @@
 # Definition for singly-linked list.
 from typing import List, Optional
+# from Patterns.linked_list.ll_0 import LinkedList
 
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -20,3 +21,43 @@ class Solution:
             currentNode.next = NewNode
             currentNode = NewNode
         return result.next
+
+class SolutionTwoPointer:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+        while curr:
+            nextTemp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextTemp
+        return prev
+
+class SolutionRecursion:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+        p = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return p
+
+
+l1 = ListNode(1)
+l1.next = ListNode(2)
+l1.next.next = ListNode(3)
+l1.next.next.next = ListNode(4)
+l1.next.next.next.next = ListNode(5)
+
+
+# while l1:
+#     print("Object: ", l1, "with: " ,l1.val)
+#     l1 = l1.next
+
+s = SolutionTwoPointer()
+result = s.reverseList(l1)
+
+
+while result:
+    print("Object: ", result, "with: " ,result.val)
+    result = result.next
