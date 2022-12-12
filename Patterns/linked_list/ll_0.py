99ae251e302f25a5c9b568fa4e3c93741254894f
Denis Mamedov <denismj87@gmail.com>
2022-12-12 11:13:45 -0600

    update ll

--- Patterns/linked_list/ll_0.py
+++ Patterns/linked_list/ll_0.py
@@ -1,29 +1,82 @@
-class LinkedList:
-    def __init__(self, data):
+
+class Node:
+    def __init__(self, data=None):
         self.data = data
         self.next = None
 
+class LinkedList:
+    def __init__(self, data=None):
+        self.head = Node(data)
+
     def append(self, data):
-        end = LinkedList(data)
-        n = self
-        while n.next != None:
-            n = n.next
-        n.next = end
-
-    def delete(self, data):
-        n = self
-        if n.data == data:
-            return n.next
-        while n.next != None:
-            if n.next.data == data:
-                n.next = n.next.next
-                return self
-            n = n.next
-        return self
-
+        new_node = Node(data)
+        cur = self.head
+        while cur.next != None:
+            cur = cur.next
+        cur.next = new_node
+    
     def print_list(self):
-        n = self
-        while n.next != None:
-            print(n.data)
-            n = n.next
-        print(n.data)
+        elements = []
+        current_node = self.head
+        while current_node.next != None:
+            current_node = current_node.next
+            elements.append(current_node.data)
+        print(elements)
+
+    def length(self):
+        cur = self.head
+        total = 0
+        while cur.next != None:
+            total += 1
+            cur = cur.next
+        return total
+
+    def get(self, index):
+        if index >= self.length():
+            print("ERROR: 'Get' Index out of range!")
+            return None
+        cur_idx = 0
+        cur_node = self.head
+        while True:
+            cur_node = cur_node.next
+            if cur_idx == index: return cur_node.data
+            cur_idx += 1
+
+    def erase(self, index):
+        if index >= self.length():
+            print("ERROR: 'Erase' Index out of range!")
+            return
+        cur_idx = 0
+        cur_node = self.head
+        while True:
+            last_node = cur_node
+            cur_node = cur_node.next
+            if cur_idx == index:
+                last_node.next = cur_node.next
+                return
+            cur_idx += 1
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+    pass
+
+def reverseList(self, head: ListNode) -> ListNode:
+    pass
+
+
+my_list = LinkedList()
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+my_list.append(4)
+
+my_list.print_list()
+
+my_list.erase(1)
+
+my_list.print_list()
