4c362d608f4208cf5fd8880b1860e8255d33f3c8
Denis Mamedov <denismj87@gmail.com>
2022-12-15 11:04:04 -0600

    lt-21 update

--- leetcode/21.py
+++ leetcode/21.py
@@ -27,6 +27,37 @@ class Solution:
             currentNode = newNode
         return result.next
 
+class SolutionTwo:
+    def mergeTwoLists(self, L1: Optional[ListNode], L2: Optional[ListNode]) -> Optional[ListNode]:
+        if L1 is None:
+            return L2
+        if L2 is None:
+            return L1
+        if L1.val < L2.val:
+            L1.next = self.mergeTwoLists(L1.next, L2)
+            return L1
+        else:
+            L2.next = self.mergeTwoLists(L1, L2.next)
+            return L2
+
+class SolutionThree:
+    def mergeTwoLists(self, L1: Optional[ListNode], L2: Optional[ListNode]) -> Optional[ListNode]:
+        res = ListNode()
+        cur = res
+        while L1 and L2:
+            if L1.val < L2.val:
+                cur.next = L1
+                L1 = L1.next
+            else:
+                cur.next = L2
+                L2 = L2.next
+            cur = cur.next
+        if L1:
+            cur.next = L1
+        elif L2:
+            cur.next = L2
+             
+        return res.next
 
 list_one = ListNode(1, ListNode(2, ListNode(4, None)))
 list_two = ListNode(9, ListNode(8, ListNode(6, None)))
