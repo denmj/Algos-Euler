373115d7fc0310caad4d697b06cdd1537eef45aa
Denis Mamedov <denismj87@gmail.com>
2022-12-19 11:14:21 -0600

    lt-160

---
+++ leetcode/160.py
@@ -0,0 +1,17 @@
+from typing import List 
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        if headA is None or headB is None:
+            return None
+        pA = headA
+        pB = headB
+        while pA != pB:
+            pA = pA.next if pA else headB
+            pB = pB.next if pB else headA
+        return pA
