2c66c10c6e40b1967ee4167f6d400cc1123b336b
Denis Mamedov <denismj87@gmail.com>
2022-12-27 11:35:31 -0600

    lt-328

---
+++ leetcode/328.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+        odd = head
+        even = head.next
+        even_head = even
+        while even and even.next:
+            odd.next = odd.next.next
+            even.next = even.next.next
+            odd = odd.next
+            even = even.next
+        odd.next = even_head
+        return head
